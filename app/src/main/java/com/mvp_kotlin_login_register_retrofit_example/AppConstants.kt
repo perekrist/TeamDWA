22271d22ed0bce10a1ed6fb77b3cfe33cf34e53a
Перегудова Кристина <perekrist@PereKristMac.local>
2020-03-12 15:15:49 +0700

    take photo

---
+++ app/src/main/java/com/mvp_kotlin_login_register_retrofit_example/AppConstants.kt
@@ -0,0 +1,6 @@
+package com.mvp_kotlin_login_register_retrofit_example
+
+object AppConstants{
+    val TAKE_PHOTO_REQUEST: Int = 2
+    val PICK_PHOTO_REQUEST: Int = 1
+}
---
+++ app/src/main/java/com/mvp_kotlin_login_register_retrofit_example/TakePhotoActivity.kt
@@ -0,0 +1,106 @@
+package com.mvp_kotlin_login_register_retrofit_example
+
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AlertDialog
+import android.widget.Toast
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import kotlinx.android.synthetic.main.activity_take_photo.*
+
+class TakePhotoActivity : AppCompatActivity() {
+
+    var fileUri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_take_photo)
+
+        takePhoto.setOnClickListener {
+            askCameraPermission()
+        }
+
+    }
+    private fun launchCamera() {
+        val values = ContentValues(1)
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
+        fileUri = contentResolver
+            .insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                values)
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if(intent.resolveActivity(packageManager) != null) {
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri)
+            intent.addFlags(
+                Intent.FLAG_GRANT_READ_URI_PERMISSION
+                        or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+            startActivityForResult(intent, AppConstants.TAKE_PHOTO_REQUEST)
+        }
+    }
+
+    fun askCameraPermission(){
+        Dexter.withActivity(this)
+            .withPermissions(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) {/* ... */
+                    if(report.areAllPermissionsGranted()){
+                        launchCamera()
+                    }else{
+                        Toast.makeText(this@TakePhotoActivity, "All permissions need to be granted to take photo", Toast.LENGTH_LONG).show()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(permissions: List<PermissionRequest>, token: PermissionToken) {/* ... */
+                    //show alert dialog with permission options
+                    AlertDialog.Builder(this@TakePhotoActivity)
+                        .setTitle(
+                            "Permissions Error!")
+                        .setMessage(
+                            "Please allow permissions to take photo with camera")
+                        .setNegativeButton(
+                            android.R.string.cancel
+                        ) { dialog, _ ->
+                            dialog.dismiss()
+                            token.cancelPermissionRequest()
+                        }
+                        .setPositiveButton(android.R.string.ok
+                        ) { dialog, _ ->
+                            dialog.dismiss()
+                            token.continuePermissionRequest()
+                        }
+                        .setOnDismissListener {
+                            token.cancelPermissionRequest()
+                        }
+                        .show()
+                }
+
+            }).check()
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int,
+                                  data: Intent?) {
+        if (resultCode == Activity.RESULT_OK
+            && requestCode == AppConstants.TAKE_PHOTO_REQUEST) {
+            imageView.setImageURI(fileUri)
+        }else if(resultCode == Activity.RESULT_OK
+            && requestCode == AppConstants.PICK_PHOTO_REQUEST){
+            //photo from gallery
+            fileUri = data?.data
+            imageView.setImageURI(fileUri)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
