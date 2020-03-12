071f1d8155a00d2390620eacbcc9a00bc70ee75e
perekrist <perekrist31@gmail.com>
2020-03-13 00:58:30 +0700

    signout

--- app/src/main/java/com/mvp_kotlin_login_register_retrofit_example/ui/SignOutFragment.kt
+++ app/src/main/java/com/mvp_kotlin_login_register_retrofit_example/ui/SignOutFragment.kt
@@ -1,5 +1,6 @@
 package com.mvp_kotlin_login_register_retrofit_example.ui
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,10 @@ import android.widget.TextView
 import android.support.v4.app.Fragment
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
+import android.content.Intent
 import com.mvp_kotlin_login_register_retrofit_example.R
+import com.mvp_kotlin_login_register_retrofit_example.view.LoginActivity
 
 class SignOutFragment : Fragment() {
 
@@ -19,5 +23,14 @@ class SignOutFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_sign_out, container, false)
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val intent = Intent(context, LoginActivity::class.java)
+        startActivity(intent)
+        val act = context as Activity
+        act.finish()
     }
 }
